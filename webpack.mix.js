d1b0d0789dd0c9566c98c1d9cfafcc4181829d91
Shivang Chaturvedi <shiva.chaturvedi91@gmail.com>
2020-07-18 00:30:46 +0530

    First Commit

---
+++ webpack.mix.js
@@ -0,0 +1,18 @@
+let mix = require("laravel-mix");
+mix.setPublicPath("public/build");
+mix.sass("vue/assets/sass/app.scss", "public/build/sass");
+
+mix.js("vue/js/main.js", "public/build");
+mix.webpackConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "vue"),
+      "@resources": path.resolve(__dirname, "resources")
+    }
+  },
+  output: {
+    filename: "[name].js",
+    chunkFilename: "js/[name].main.js",
+    publicPath: "/build/"
+  }
+});
