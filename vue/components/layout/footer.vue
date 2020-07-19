1ea8e2ea079ea1d74d30dcaaaf1952ad1d40352a
Shivang Chaturvedi <shiva.chaturvedi91@gmail.com>
2020-07-19 18:11:23 +0530

    Footer Adjustments

--- vue/components/layout/footer.vue
+++ vue/components/layout/footer.vue
@@ -1,36 +1,38 @@
 <template>
-  <nav class="navbar is-fixed-bottom is-transparent">
-    <div class="navbar-menu">
-      <div class="navbar-end">
-        <div class="navbar-item">&copy; {{year}}, Shivang Chaturvedi</div>
-        <div class="navbar-item">
+  <footer v-show="isFooterBarVisible" class="footer">
+    <div class="container-fluid">
+      <div class="level">
+        <div class="level-left">
+          <div class="level-item">
+            <div class="logo">
+              <a class="navbar-item" href="https://github.com/shiva2021">
+                <img
+                  src="/footer_logo.svg"
+                  alt="Bulma: a modern CSS framework based on Flexbox"
+                  width="112"
+                  height="28"
+                />
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="level-right">
+          <div class="level-item">
             <a href="https://www.facebook.com/shivang.chaturvedi.56/" target="_blank">
-                <img src="/assets/img/facebook.svg" width="20" >
+              <img src="/assets/img/facebook.svg" width="20" class="mr-10" />
             </a>
-        </div>
-        <div class="navbar-item">
             <a href="https://www.instagram.com/sh_i_vang/" target="_blank">
-                <img src="/assets/img/instagram.svg" width="20" >
+              <img src="/assets/img/instagram.svg" width="20" class="mr-10" />
             </a>
-        </div>
-        <div class="navbar-item">
             <a href="https://twitter.com/Shivang2191?s=09" target="_blank">
-                <img src="/assets/img/twitter.svg" width="20" >
+              <img src="/assets/img/twitter.svg" width="20" />
             </a>
-        </div>
-        <div class="navbar-brand">
-          <a class="navbar-item" href="https://github.com/shiva2021">
-            <img
-              src="/Artboard.svg"
-              alt="Bulma: a modern CSS framework based on Flexbox"
-              width="112"
-              height="28"
-            />
-          </a>
+          </div>
+          <div class="level-item">&copy; {{year}}, Shivang Chaturvedi</div>
         </div>
       </div>
     </div>
-  </nav>
+  </footer>
 </template>
 
 <script>
